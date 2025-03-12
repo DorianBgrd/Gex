02feecc7c3fce2a258b457250e83a4553c45953a
DorianBgrd <bougard.dorian@gmail.com>
2025-03-12 21:50:07 +0100

    Using wrappers function.

--- Gex/python/module.cpp
+++ Gex/python/module.cpp
@@ -1,13 +1,6 @@
-#include "Gex/include/Attribute_Wrap.h"
-#include "Gex/include/Graph_Wrap.h"
-#include "Gex/include/Status_Wrap.h"
-#include "Gex/include/Node_Wrap.h"
-#include "Gex/include/io_Wrap.h"
-#include "Gex/include/PluginLoader_Wrap.h"
-#include "Gex/include/NodeFactory_Wrap.h"
-#include "Gex/include/References_Wrap.h"
-#include "Gex/include/UndoStack_Wrap.h"
-#include "Gex/include/Profiler_Wrap.h"
+#include "Gex/include/Wrappers.h"
+
+#include "Tsys/include/tsys.h"
 
 #include "boost/python.hpp"
 
@@ -32,33 +25,15 @@ boost::python::object GetAvailableTypes(
 
 BOOST_PYTHON_MODULE(Gex_Python)
 {
-    boost::python::def("GetAvailableTypes", boost::python::raw_function(GetAvailableTypes));
 
-    Gex::Python::Feedback_Wrap::RegisterPythonWrapper();
+    boost::python::def(
+            "GetAvailableTypes",
+            boost::python::raw_function(
+                    GetAvailableTypes
+            )
+    );
 
-    Gex::Python::Attribute_Wrap::RegisterPythonWrapper();
 
-    Gex::Python::GraphContext_Wrap::RegisterPythonWrapper();
+    Gex::Python::RegisterPythonWrappers();
 
-    Gex::Python::Node_Wrap::RegisterPythonWrapper();
-
-    Gex::Python::CompoundNode_Wrap::RegisterPythonWrapper();
-
-    Gex::Python::PluginLoader_Wrap::RegisterPythonWrapper();
-
-    Gex::Python::NodeBuilder_Wrap::RegisterPythonWrapper();
-
-    Gex::Python::NodeFactory_Wrap::RegisterPythonWrapper();
-
-    Gex::Python::References_Wrap::RegisterPythonWrapper();
-
-    Gex::Python::IO::RegisterPythonWrapper();
-
-    Gex::Python::UndoStack_Wrap::RegisterPythonWrapper();
-
-    Gex::Python::Event_Wrap::RegisterPythonWrapper();
-
-    Gex::Python::EvaluationProfiler_Wrap::RegisterPythonWrapper();
-
-    Gex::Python::EvaluationNodeProfiler_Wrap::RegisterPythonWrapper();
 }
